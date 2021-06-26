41b8830020fcb51b3f6d586eb90cb81816c4840a
JoseVieira1996 <jose.adm@outlook.com>
2021-06-26 17:56:01 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/Dspesquisa.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dspesquisa {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dspesquisa.class, args);
+	}
+
+}
